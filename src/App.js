51ea3e4364c5f6e696b7e157f74c376c02b8dcaa
riteshdange <51057157+riteshdange@users.noreply.github.com>
2025-08-01 00:42:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
